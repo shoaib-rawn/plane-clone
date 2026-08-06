281f5ab5ac2bc39764c3ff9e57ce525dbc5866a5
HammasJaved <hammasdevjaved@gmail.com>
2026-08-06 11:48:15 +0500

    feat(web): initialize React frontend setup

---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import MainLayout from "../layouts/MainLayout";
+
+function Home() {
+  return (
+    <MainLayout>
+      <h1>Welcome to MiniPlane</h1>
+    </MainLayout>
+  );
+}
+
+export default Home;
